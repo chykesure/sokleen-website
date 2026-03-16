5c8ef8cbbea6d7557af196c73667eaab2930c734
Polycarp Chike Pascal <polycarpchike@gmail.com>
2026-03-16 13:24:58 +0100

    new next config.js file created

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // your config options here, e.g.
+  images: {
+    domains: ['example.com'],
+  },
+  // ... other settings
+};
+
+module.exports = nextConfig;
